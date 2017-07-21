041ff202759f835a3422d70719ae92cd7a98a000
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-21 01:42:21 +0000

    [tx-robot] updated from transifex

--- l10n/fa.js
+++ l10n/fa.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "یادداشت ها",
     "New note" : "یادداشت جدید",
+    "Note is currently saving. Leaving the page will delete all changes!" : "یادداشت در حال ذخیره‌شدن است. خروج از صفحه باعث از بین رفتن تغییرات انجام شده می‌شود.",
     "_%n word_::_%n words_" : ["%n کلمه"],
     "Delete note" : "حذف یادداشت",
     "Favorite" : "برگزیده"
