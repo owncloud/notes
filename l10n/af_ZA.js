17540f1eb02709110c6ce78bcdbb6614edb3991f
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-12 01:42:19 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "notes",
+    {
+    "Notes" : "Notas",
+    "New note" : "Nuwe nota",
+    "Note is currently saving. Leaving the page will delete all changes!" : "Nota word tans bewaar. Deur die blad te verlaat word alle veranderinge ontdaan!",
+    "_%n word_::_%n words_" : ["%n woord","%n woorde"],
+    "Delete note" : "Skrap nota",
+    "Favorite" : "Gunsteling"
+},
+"nplurals=2; plural=(n != 1);");
