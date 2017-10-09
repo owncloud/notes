b951f3ca322e1db84584e0b821e18c1b6dd37418
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-09 08:22:20 +0000

    [tx-robot] updated from transifex

--- l10n/hu_HU.js
+++ l10n/hu_HU.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "Jegyzetek",
     "New note" : "Új jegyzet",
+    "Note is currently saving. Leaving the page will delete all changes!" : "A jegyzet jelenleg mentés alatt van. Az oldal elhagyása törli az összes változtatást!",
     "_%n word_::_%n words_" : ["%n szó","%n szó"],
     "Delete note" : "Jegyzet törlése",
     "Favorite" : "Kedvenc"
