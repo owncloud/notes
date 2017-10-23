5fb71443955bd9703838e389cef738b72e00a2a7
ownCloud Jenkins <jenkins@owncloud.com>
2017-10-23 01:42:22 +0000

    [tx-robot] updated from transifex

--- l10n/zh_CN.js
+++ l10n/zh_CN.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Notes" : "笔记",
     "New note" : "新建笔记",
+    "Note is currently saving. Leaving the page will delete all changes!" : "正在保存笔记，离开页面将会放弃所有更改！",
     "_%n word_::_%n words_" : ["%n 个字"],
     "Delete note" : "删除笔记",
     "Favorite" : "收藏"
