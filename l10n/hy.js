1a6a58a343d35daac12dd4f7479aba3834c66a6e
ownCloud Jenkins <jenkins@owncloud.com>
2018-03-15 01:43:19 +0000

    [tx-robot] updated from transifex

--- l10n/hy.js
+++ l10n/hy.js
@@ -5,6 +5,7 @@ OC.L10N.register(
     "New note" : "Նոր նոթ",
     "Note is currently saving. Leaving the page will delete all changes!" : "Նոթը այժմ պահվում է: Էջը լքելը կչեղարկի բոլոր փոփոխությունները:",
     "_%n word_::_%n words_" : ["%n բառ","%n բառ"],
-    "Delete note" : "Ջնջել նոթը"
+    "Delete note" : "Ջնջել նոթը",
+    "Favorite" : "Սիրված"
 },
 "nplurals=2; plural=(n != 1);");
--- l10n/nn_NO.js
+++ l10n/nn_NO.js
@@ -3,6 +3,9 @@ OC.L10N.register(
     {
     "Notes" : "Notat",
     "New note" : "Nytt notat",
-    "Delete note" : "Slett notat"
+    "Note is currently saving. Leaving the page will delete all changes!" : "Lagring av notat pågår. Hvis du forlet sida no vil alle endringar slettast!",
+    "_%n word_::_%n words_" : ["%n ord","%n ord"],
+    "Delete note" : "Slett notat",
+    "Favorite" : "Favoritt"
 },
 "nplurals=2; plural=(n != 1);");
