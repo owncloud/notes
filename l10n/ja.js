625d2c4b3bbe4d1e09b1334bd6a7613e3bee7c21
ownClouders <devops@owncloud.com>
2018-11-26 07:10:26 +0000

    [tx] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "notes",
     {
     "New note" : "新しいノート",
-    "Note is currently saving. Leaving the page will delete all changes!" : "ノートを保存しています。このページから移動する場合は全ての変更が削除されます！",
+    "Note is currently saving. Leaving the page will delete all changes!" : "ノートを保存しています。このページから移動すると全ての変更が失われます！",
     "_%n word_::_%n words_" : ["%n 単語"],
     "Delete note" : "ノートを削除",
     "Favorite" : "お気に入り"
