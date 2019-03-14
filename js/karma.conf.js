52cab4e0fa76ee4a2b7e0203f8330ff6e1ebe9cd
Phil Davis <phil@jankaritech.com>
2019-03-14 09:51:59 +0545

    Implement CI on drone

--- js/karma.conf.js
+++ js/karma.conf.js
@@ -77,7 +77,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Firefox'],
+        browsers: ['PhantomJS'],
 
         // If browser does not capture in given timeout [ms], kill it
         captureTimeout: 5000,
