78d911f907e328721c2ab168cc3bd8f866f11ca7
Thomas Müller <thomas.mueller@tmit.eu>
2020-08-04 01:38:58 +0000

    [tx] updated from transifex

--- l10n/ar.js
+++ l10n/ar.js
@@ -1,9 +1,9 @@
 OC.L10N.register(
     "notes",
     {
-    "Notes" : "الملاحظات",
     "New note" : "ملاحظة جديدة ",
-    "Note is currently saving. Leaving " : "يتم الآن حفظ الملاحظة. جاري الخروج",
-    "Delete note" : "حذف الملاحظة "
+    "Note is currently saving. Leaving the page will delete all changes!" : "يتم حفظ الملاحظة حاليًا. سيؤدي مغادرة الصفحة إلى حذف كل التغييرات!",
+    "Delete note" : "حذف الملاحظة ",
+    "Favorite" : "المفضلة"
 },
 "nplurals=6; plural=n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5;");
