01c8c05a1ce3fdd4bbb7acd2f760785a655c50ad
Thomas Müller <thomas.mueller@tmit.eu>
2020-11-01 00:05:57 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "notes",
+    {
+    "New note" : "Neue Notiz",
+    "Note is currently saving. Leaving the page will delete all changes!" : "Notizen werden gerade gespeichert. Das Verlassen der Seite löscht alle Änderungen!",
+    "_%n word_::_%n words_" : ["%n Wort","%n Wörter"],
+    "Delete note" : "Notiz löschen",
+    "Favorite" : "Favorisieren"
+},
+"nplurals=2; plural=(n != 1);");
