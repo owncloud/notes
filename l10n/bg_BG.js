e6f395301655efac43ddf7a0db0cbbdf82bdb3b6
ownClouders <devops@owncloud.com>
2022-12-02 01:37:35 +0000

    [tx] updated from transifex

--- l10n/bg_BG.js
+++ l10n/bg_BG.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "notes",
     {
     "New note" : "Нова бележка",
-    "Note is currently saving. Leaving the page will delete all changes!" : "В момента се запазва бележка. Напускането на тази страница ще изтрие всички промени!",
+    "Note is currently saving. Leaving the page will delete all changes!" : "Бележката се запазва в момента. Напускането на страницата ще изтрие всички промени!",
     "_%n word_::_%n words_" : ["%n дума","%n думи"],
     "Delete note" : "Изтриване на бележка",
     "Favorite" : "Любими"
