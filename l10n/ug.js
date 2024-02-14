e4fdcf39e59070dc9632d950caa07d62e158a953
ownClouders <devops@owncloud.com>
2024-02-14 08:11:57 +0000

    [tx] updated from transifex

---
+++ l10n/ug.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "notes",
+    {
+    "New note" : "يېڭى خاتىرە",
+    "Note is currently saving. Leaving the page will delete all changes!" : "خاتىرە نۆۋەتتە ساقلىنىۋاتىدۇ. بۇ بەتتىن چىكىنگەندە بارلىق ئۆزگەرتىشلەر ئۆچۈرۈلىدۇ!",
+    "Delete note" : "خاتىرىنى ئۆچۈر",
+    "Favorite" : "ئامراق"
+},
+"nplurals=2; plural=(n != 1);");
