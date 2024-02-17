d309fe2c6e8c3678fda0f5e7bbc6187eb2bbd625
ownClouders <devops@owncloud.com>
2024-02-17 00:47:27 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -3,8 +3,8 @@ OC.L10N.register(
     {
     "New note" : "הערה חדשה",
     "Note is currently saving. Leaving the page will delete all changes!" : "הערה נשמרת ברגע זה. יציאה מהדף תמחק את כל השינויים!",
-    "_%n word_::_%n words_" : ["%n מילה","%n מילים","%n מילים","%n מילים"],
+    "_%n word_::_%n words_" : ["%n מילה","%n מילים","%n מילים"],
     "Delete note" : "מחיקת הערה",
     "Favorite" : "מועדף"
 },
-"nplurals=4; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
