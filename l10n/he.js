4a14f6101050023be40a2de37fbad5e7a0a780e3
ownClouders <devops@owncloud.com>
2024-03-09 00:05:24 +0000

    [tx] updated from transifex

--- l10n/he.js
+++ l10n/he.js
@@ -7,4 +7,4 @@ OC.L10N.register(
     "Delete note" : "מחיקת הערה",
     "Favorite" : "מועדף"
 },
-"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: (n % 10 == 0 && n % 1 == 0 && n > 10) ? 2 : 3;");
+"nplurals=3; plural=(n == 1 && n % 1 == 0) ? 0 : (n == 2 && n % 1 == 0) ? 1: 2;");
